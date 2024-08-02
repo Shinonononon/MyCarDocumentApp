a051d4df5df74bf5ae2b633eea5610c8ed17fc44
Shinonononon <h1karu.sn0928@gmail.com>
2024-08-02 14:08:32 +0900

    取り敢えずデプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-server "3.113.29.248",
+server "mycardocumentapp.com",
   user: "ec2-user",
   roles: %w{web db app},
   ssh_options: {
