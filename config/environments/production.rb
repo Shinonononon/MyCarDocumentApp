2f96c733f0c5bdc4fb105f0247b1a7d7bf9d8db0
Shinonononon <h1karu.sn0928@gmail.com>
2024-08-02 15:02:55 +0900

    お願いします2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,5 +117,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_controller.default_url_options = { host: 'mycardocumentapp.com', port: 80 }
+  config.action_controller.default_url_options = { host: 'mycardocumentapp.com', protcol: 'https' }
 end
